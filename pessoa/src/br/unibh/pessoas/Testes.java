ae200c8afd455c31af91c94e51913e083ca7cdda
11411556 <11411556@ESTB80712.unibh.edu>
2015-09-21 20:50:22 -0300

    Aula 21/09

--- pessoa/src/br/unibh/pessoas/Testes.java
+++ pessoa/src/br/unibh/pessoas/Testes.java
@@ -1,30 +1,55 @@
 package br.unibh.pessoas;
 
-
+import java.util.Date;
 import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 import br.unibh.pessoas.entidades.PessoaFisica;
 import br.unibh.pessoas.persistencia.PessoaFisicaDAO;
 
-
 public class Testes {
-	
+
 	@Test
-	public void testePessoaFisicaFindaAll(){
-		
+	public void testePessoaFisicaFindaAll() {
+
 		PessoaFisicaDAO dao = new PessoaFisicaDAO();
-		List<PessoaFisica> l =  dao.findAll();
+		List<PessoaFisica> l = dao.findAll();
 		Assert.assertEquals(l.size(), 100);
 	}
-	
+
 	@Test
-	public void testePessoaFisicaFind(){
+	public void testePessoaFisicaFind() {
 		PessoaFisicaDAO dao = new PessoaFisicaDAO();
 		PessoaFisica p = dao.find(6L);
-		Assert.assertEquals(p.getNome(),"Maya S. Logan");
-		
+		Assert.assertEquals(p.getNome(), "Maya S. Logan");
+
 	}
 
+	public void testePessoaFisicaInserir() {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		PessoaFisica pf = new PessoaFisica(null, "Fulano da Silva", "Rua A", "3133334444", "12345678911",
+				"fulano@gmail.com", new Date(), "M");
+		dao.insert(pf);
+		PessoaFisica pf2 = dao.find("Fulano da Silva");
+		Assert.assertNotNull(pf2);
 
+	}
+	
+
+	@Test
+	public void testePessoaFisicaInserirExcluir (){
+		
+		PessoaFisicaDAO dao = new PessoaFisicaDAO ();
+		PessoaFisica pf = new PessoaFisica(null, "Fulano da Silva", "Rua A", "3133333333", "11122233344",
+				"fulano@gmail.com", new Date(), "M");
+				dao.insert(pf);
+		PessoaFisica pf2 = dao.find("Fulano da Silva");
+		Assert.assertNotNull(pf2);
+		PessoaFisica pf3 = dao.find("Fulano da Silva");
+		Assert.assertNull(pf3);
+	}
+	
+	
 }
--- pessoa/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
+++ pessoa/src/br/unibh/pessoas/persistencia/PessoaFisicaDAO.java
@@ -2,12 +2,15 @@ package br.unibh.pessoas.persistencia;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.unibh.pessoas.entidades.PessoaFisica;
 
 public class PessoaFisicaDAO implements DAO<PessoaFisica, Long> {
+
+	private SimpleDateFormat df = new SimpleDateFormat();
 
 	@Override
 	public PessoaFisica find(Long id) {
@@ -18,10 +21,9 @@ public class PessoaFisicaDAO implements DAO<PessoaFisica, Long> {
 			p.setLong(1, id);
 			ResultSet res = p.executeQuery();
 			if (res.next()) {
-				return new PessoaFisica(
-				res.getLong("id"), res.getString("nome"), res.getString("endereco"), res.getString("telefone"),
-						res.getString("cpf"), res.getString("email"), res.getDate("data_nascimento"),
-						res.getString("sexo"));
+				return new PessoaFisica(res.getLong("id"), res.getString("nome"), res.getString("endereco"),
+						res.getString("telefone"), res.getString("cpf"), res.getString("email"),
+						res.getDate("data_nascimento"), res.getString("sexo"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -33,43 +35,17 @@ public class PessoaFisicaDAO implements DAO<PessoaFisica, Long> {
 		return null;
 	}
 
-	@Override
-	public void insert(PessoaFisica t) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void update(PessoaFisica t) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void delete(PessoaFisica t) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<PessoaFisica> findAll() {
-
-		List<PessoaFisica> lista = new ArrayList<PessoaFisica>();
+	public PessoaFisica find(String nome) {
 		try {
-
-			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_pessoa_fisica").executeQuery();
-
-			while (res.next())
-				;
-			lista.add(
-
-			new PessoaFisica(
-
-			res.getLong("id"), res.getString("nome"), res.getString("endereco"), res.getString("telefone"),
-					res.getString("cpf"), res.getString("email"), res.getDate("data_nascimento"), res.getString("sexo"))
-
-			);
-
+			PreparedStatement p = JDBCUtil.getConnection()
+					.prepareStatement("select * from tb_pessoa_fisica where nome like ?");
+			p.setString(1, nome + "%");
+			ResultSet res = p.executeQuery();
+			if (res.next()) {
+				return new PessoaFisica(res.getLong("id"), res.getString("nome"), res.getString("endereco"),
+						res.getString("telefone"), res.getString("cpf"), res.getString("email"),
+						res.getDate("data_nascimento"), res.getString("sexo"));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 
@@ -77,6 +53,86 @@ public class PessoaFisicaDAO implements DAO<PessoaFisica, Long> {
 
 		}
 
+		return null;
+	}
+
+	// Inserir no banco
+	@Override
+	public void insert(PessoaFisica t) {
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("insert into tb_pessoa_fisica "
+					+ "(nome, endereco, telefone, cpf, email, data_nascimento, sexo) values (?,?,?,?,?,?,?)");
+			p.setString(1, t.getNome());
+			p.setString(2, t.getEndereco());
+			p.setString(3, t.getTelefone());
+			p.setString(4, t.getCpf());
+			p.setString(5, t.getEmail());
+			p.setString(6, df.format(t.getDataNascimento()));
+			p.setString(7, t.getSexo());
+			p.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+	}
+
+	// Update no banco
+	@Override
+	public void update(PessoaFisica t) {
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("update tb_pessoa_fisica "
+					+ "set nome = ?, endereco = ?, telefone = ?, cpf = ?, email = ?, data_nascimento = ?, sexo = ? where id = ?");
+			p.setString(1, t.getNome());
+			p.setString(2, t.getEndereco());
+			p.setString(3, t.getTelefone());
+			p.setString(4, t.getCpf());
+			p.setString(5, t.getEmail());
+			p.setString(6, df.format(t.getDataNascimento()));
+			p.setString(7, t.getSexo());
+			p.setLong(8, t.getId());
+			p.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+	}
+
+	// Deletar no banco
+	@Override
+	public void delete(PessoaFisica t) {
+		try {
+			PreparedStatement p = JDBCUtil.getConnection()
+					.prepareStatement("delete from tb_pessoa_fisica where id = ?");
+			p.setLong(1, t.getId());
+			p.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+	}
+
+	// Localizar tudo
+	@Override
+	public List<PessoaFisica> findAll() {
+		List<PessoaFisica> lista = new ArrayList<PessoaFisica>();
+		try {
+			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_pessoa_fisica").executeQuery();
+			while (res.next()) {
+				lista.add(
+					new PessoaFisica(
+							res.getLong("id"), res.getString("nome"), res.getString("endereco"), res.getString("telefone"),
+							res.getString("cpf"), res.getString("email"), res.getDate("data_nascimento"),
+							res.getString("sexo"))
+				);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
 		return lista;
 	}
 
